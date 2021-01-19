e449b6c33807e8978afb65568bd4c68756ac60cf
kumochan <thaotrinhminh@gmail.com>
2021-01-19 12:49:31 +0700

    checkall

---
+++ lession-03/angular-overview/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-overview';
+
+  months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+  //isavailable = false;
+
+  myClickFunction(event: any) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Button is clicked");
+    //document.write(this.months[0]);
+    console.log(this);
+  }
+
+  
+
+  
+  
+}
