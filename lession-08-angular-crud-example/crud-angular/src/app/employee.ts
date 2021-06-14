c8a520f812aa376f87b86214a2d2e97493b1dbc2
kumochan <thaotrinhminh@gmail.com>
2021-06-14 11:36:25 +0700

    update folder

--- lession-08-angular-crud-example/crud-angular/src/app/employee.ts
+++
@@ -1,6 +0,0 @@
-export class Employee {
-    id!: number;
-    first_name!: string;
-    last_name!: string;
-    created_at!: any;
-}
--- typescript-demo/p.js
+++
@@ -1,6 +0,0 @@
-var promise = new Promise(function (resolve, reject) {
-    resolve('done');
-    reject(new Error('…')); // ignored
-    setTimeout(function () { return resolve('…'); }); // ignored
-});
-promise.then(function (data) { return console.log(data); });
